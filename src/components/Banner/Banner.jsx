4fbad7b6a4a4b312a2378682a90cfd0b89d90434
Joseph Cotza <joseph.cotza.01@gmail.com>
2022-08-24 16:15:30 +0200

    retrait des repertoires images frontend

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = (image) => {
+    console.log(image)
+    return (
+        <div className='Banner'>
+            batiments1
+            <img src='images/batiments1.png' alt='' />
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+const Header = () => {
+    return (
+        <div className='Header'>
+            <Banner image={'batiments1.png'}/>
+        </div>
+    );
+};
+
+export default Header;
